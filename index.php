164b0f5d116a7dda55050468106b48031326c934
meeleh <47509537+meeleh@users.noreply.github.com>
2019-06-06 23:01:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+//Non toccare qui
+$api = $_GET['api'];
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+/////////////////////////////////////////////
+
+
+///////////////////// Parte da api.telegram.org //////////////////////////
+
+//Variabili
+$msg = $update["message"]["text"];
+$chatID = $update["message"]["chat"]["id"];
+
+//Funzioni
+function sm($chatID, $text) {
+global $api;
+$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text);  
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+
+//Azioni
+if($msg == "/start") {
+sm($chatID, "Proviamo");
+}
+?>
---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+$token = "796042247:AAEW581EaI5x0995-HnXjhP5mdxXas58rYA"; //Il token da @BotFather
+$sito = "https://git.heroku.com/ohhnoobot.git/index.php"; //Il percorso dell'index
+
+//////////////////////////////////// NON TOCCARE QUI ////////////////////////////////////////
+$webhook = 'https://api.telegram.org/bot'.$token.'/setwebhook?url='.$sito.'?api=bot'.$token;
+$ch = curl_init("$webhook");
+curl_exec($ch);
