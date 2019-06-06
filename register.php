bd14d83f65f45573f0f34ab646890287fddb92d4
meeleh <47509537+meeleh@users.noreply.github.com>
2019-06-06 23:07:21 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 $token = "796042247:AAEW581EaI5x0995-HnXjhP5mdxXas58rYA"; //Il token da @BotFather
-$sito = "https://ohhnoobot.herokuapp.com//index.php"; //Il percorso dell'index
+$sito = "https://ohhnoobot.herokuapp.com/index.php"; //Il percorso dell'index
 
 //////////////////////////////////// NON TOCCARE QUI ////////////////////////////////////////
 $webhook = 'https://api.telegram.org/bot'.$token.'/setwebhook?url='.$sito.'?api=bot'.$token;
