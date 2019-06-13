c931894b388932191ecba6909cfabaa78522b79e
meeleh <47509537+meeleh@users.noreply.github.com>
2019-06-13 22:39:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,13 +22,6 @@ else {
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text); }
 }
 
-/*function sendphoto($chatID, $photo, $caption) {
-global $api;
-
-file_get_contents('https://api.telegram.org/'.$api.'/sendPhoto?chat_id='.$chatID.'&photo='.$photo.'&caption='.urlencode($caption));  
-}
-*/
-
 function sf($chatID, $photo, $reply) {
 global $api;
 global $update;
@@ -51,14 +44,13 @@ if(array_key_exists("text", $msg)){
 	$out = sf($chatID, $photo, NULL);}
 	    //$out = sm($chatID, "Ehi ehi", NULL); 
 	
-    if($text =="ciao" or $text == "ehi") 
-       $out = sm($chatID,"Ciao!", $user_id);
+    if($text =="ciao" or $text == "ehi") { 
+       $out = sm($chatID,"Ciao!", $user_id);  }
 	
     if($text == "Foto"){
 	$photo = 'https://www.miciogatto.it/new/wp-content/uploads/2018/02/Linguaggio-dei-gatti-come-capire-un-gatto-1030x587.jpg';
 	$out = sf($chatID, $photo, NULL);}
-	//if ($text == "Foto")
-	//sendphoto($chatID,'https://www.miciogatto.it/new/wp-content/uploads/2018/02/Linguaggio-dei-gatti-come-capire-un-gatto-1030x587.jpg', "Questa è una foto");
+	
 	
 }
 
