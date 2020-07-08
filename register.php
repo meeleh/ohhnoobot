00500b9641991d65d8cdccc51dde947be42d1ab6
meeleh <47509537+meeleh@users.noreply.github.com>
2020-07-08 21:23:56 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-$token = "1071068150:AAF5tebVKxqndoE9xN9Tm6Q5rYLZKY5KD60"; //Il token da @BotFather
+$token = "1145887993:AAGmVvRibrgjacU8E8fDpQVIXbv0B3n8KqY"; //Il token da @BotFather
 $sito = "https://ohhnoobot.herokuapp.com/index.php"; //Il percorso dell'index
 
 //////////////////////////////////// NON TOCCARE QUI ////////////////////////////////////////
