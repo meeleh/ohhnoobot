3c51160e14cf3402a29ad0c67eca4664a1517d07
meeleh <47509537+meeleh@users.noreply.github.com>
2020-07-09 08:21:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(array_key_exists("text", $msg)){
         {if (stripos($text, "bella") !== false)
        { $out = sm($chatID,"Sono io!", $user_id);  }}
 
-     if ($text["reply_to_message"]["from"]["id"] == "1145887993") {
+     if ($text["reply_to_message"] == "1145887993") {
         $out = sm($chatID,"eheh".$user."!", $user_id);  }
 
 	
