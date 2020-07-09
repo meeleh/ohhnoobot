7569efb888118d4d49b1bf679a9e443c17f70f35
meeleh <47509537+meeleh@users.noreply.github.com>
2020-07-09 20:07:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if(array_key_exists("text", $msg)){
     $text = strtolower($update["message"]["text"]);
 	
     if($text == "/start") {
-	$out = sm($chatID, "Che vuoi?", NULL); }
+	$out = sm($chatID, "Che vuoi??", NULL); }
 	
     if($text =="ciao" or $text == "ehi") { 
        $out = sm($chatID,"Ciao ".$user."!", $user_id);  }
@@ -52,7 +52,7 @@ if(array_key_exists("text", $msg)){
           if (stripos($text, "suca") !== false)
            { $rand = array("mmm lo suca", "levati", "nnnn");
           
-             $out = sm($chatID, print_r($rand[array_rand($rand)]), $user_id);  }
+             $out = sm($chatID, ($rand[array_rand($rand)]), $user_id);  }
           
 
 }
