da2868c9ad172876cf6c02d5c85251e994b39d92
meeleh <47509537+meeleh@users.noreply.github.com>
2020-07-09 23:47:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if(array_key_exists("text", $msg)){
              $out = sm($chatID, ($rand[array_rand($rand)]), NULL);  }
 
         if (stripos($text, "tette") !== false)
-          { $rand = array("Ho le tettone", "mmm a chi le esco?", "Ragazzi, seguite @quetetas", "Ragazzi, seguite @tettinepiccole", "Succhiamele", NULL, NULL, NULL);
+          { $rand = array("Ho le tettone", "mmm a chi le esco?", "puoi anche essere donna ma se non hai le tettone sei solo un mostro deforme che non deve assolutamente parlare.", "Le tette sono l'antistress per eccellenza", "Succhiamele", NULL, NULL, NULL);
           
              $out = sm($chatID, ($rand[array_rand($rand)]), NULL);  }
 
