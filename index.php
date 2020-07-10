097a2d51a2c21c72c347cb6683090654703eea10
meeleh <47509537+meeleh@users.noreply.github.com>
2020-07-10 09:33:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -134,7 +134,9 @@ if(array_key_exists("text", $msg)){
              $out = sm($chatID, ($rand[array_rand($rand)]), $user_id);  }
 
           if ((stripos($text, "gnocca") !== false) or (stripos($text, "carina") !== false) or  (stripos($text, "sesso") !== false) or (stripos($text, "scopo") !== false)) 
-           { $rand = array("graziee", "lo so hahah", "levati", "hahahaha", "baciami");} 
+           { $rand = array("graziee", "lo so hahah", "levati", "hahahaha", "baciami");
+           $out = sm($chatID, ($rand[array_rand($rand)]), $user_id);} 
+          } 
          
             if ((stripos($text, "cagna") !== false) or (stripos($text, "odio") !== false) or (stripos($text, "puttana") !== false) or (stripos($text, "zoccola") !== false)) 
            { $rand = array("caso umano", "sono io", "puoi stare zitto? ", "taci");
