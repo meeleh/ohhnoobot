10a55976f079cd973ad1dca1a5278543c4a8c2c7
meeleh <47509537+meeleh@users.noreply.github.com>
2021-09-07 19:18:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -190,7 +190,7 @@ Else
 	$out = sm($chatID, "Andiamo a bere qualcosa", NULL); }
 	
 	elseif (stripos($text, "calcio") !== false){
-	$out = sm($chatID, "Chi usa altre gif di calcio è pregato di uscire o provvederemo alle dovute sanzioni", "vietato parlare di calcio", NULL, NULL, NULL, NULL, NULL); }
+	$out = sm($chatID, "Chi usa altre gif di calcio è pregato di uscire o provvederemo alle dovute sanzioni", "vietato parlare di calcio", "basta calciooo", "maschi tutti noiosi assurdo", NULL, NULL, NULL, NULL, NULL); }
 
         elseif (stripos($text, "latte") !== false)
           { $rand = array("chi devo allattare?", "mmm che sete", NULL , NULL, NULL, NULL);
